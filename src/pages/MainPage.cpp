f00a96358bce0c0fd61281b539144c2ffab23660
magnuax <magaxelsen@gmail.com>
2026-03-04 09:48:16 +0100

    UPDATE: Include SectorDetailsPanel in MainPage and update placeholder gridsizes

--- src/pages/MainPage.cpp
+++ src/pages/MainPage.cpp
@@ -3,21 +3,27 @@
 #include "pages/MainPage.hpp"
 #include "panels/StateGridPanel.hpp"
 #include "panels/OperatorPanel.hpp"
+#include "panels/SectorDetailsPanel.hpp"
 
 MainPage::MainPage(QWidget *parent) : QWidget(parent)
 {
     QHBoxLayout *mainLayout = new QHBoxLayout(this);
+    
+    mainLayout->addWidget(createSectorDetailsPanel());
     mainLayout->addWidget(createStateGrid());
     mainLayout->addWidget(createOperatorPanel());
-    mainLayout->setStretch(0, 3); 
-    mainLayout->setStretch(1, 1);
+    
+    mainLayout->setStretch(0, 1);
+    mainLayout->setStretch(1, 3);
+    mainLayout->setStretch(2, 1);
+    
     setLayout(mainLayout);
 }
 
 QWidget *MainPage::createStateGrid()
 {
-    int PLACEHOLDER_rows = 10;
-    int PLACEHOLDER_cols = 10;
+    int PLACEHOLDER_rows = 30;
+    int PLACEHOLDER_cols = 30;
 
     QWidget *stateGrid = new StateGridPanel(
         PLACEHOLDER_rows,
@@ -33,4 +39,10 @@ QWidget *MainPage::createOperatorPanel()
 {
     QWidget *operatorPanel = new OperatorPanel(this);
     return operatorPanel;
+}
+
+QWidget *MainPage::createSectorDetailsPanel()
+{
+    QWidget *sectorDetailsPanel = new SectorDetailsPanel(this);
+    return sectorDetailsPanel;
 }
