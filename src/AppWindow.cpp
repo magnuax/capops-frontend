391218a032861bb26a021d68070527c95c9395fe
magnuax <magaxelsen@gmail.com>
2026-03-04 09:33:11 +0100

    UPDATE: set _pageStack as central widget

--- src/AppWindow.cpp
+++ src/AppWindow.cpp
@@ -15,6 +15,8 @@ void AppWindow::initializePages()
     _mainPage = new MainPage(this);
     _pageStack = new QStackedWidget(this);
     _pageStack->addWidget(_mainPage);
+
+    setCentralWidget(_pageStack);
 }
 
 void AppWindow::showMainPage()
