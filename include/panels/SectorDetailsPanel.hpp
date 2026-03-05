8ca89e80ccdb5a042a121201a1a5d0c8a7014d6b
magnuax <magaxelsen@gmail.com>
2026-03-05 13:23:14 +0100

    UPDATE: store selected sector in details panel, update with placeholder info

--- include/panels/SectorDetailsPanel.hpp
+++ include/panels/SectorDetailsPanel.hpp
@@ -2,10 +2,24 @@
 
 #include <QWidget>
 
+class QPoint;
+class QLabel;
+class QTabWidget;
+
 class SectorDetailsPanel : public QWidget
 {
     Q_OBJECT
 
 public:
     explicit SectorDetailsPanel(QWidget *parent = nullptr);
+
+public slots:
+    void setSector(int row, int col);
+
+private:
+    QTabWidget* createTabs();
+
+    QLabel* _colLabel = nullptr;
+    QLabel* _rowLabel = nullptr;
+    QPoint _selectedSector = {0, 0};
 };
