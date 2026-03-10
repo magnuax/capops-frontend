deea107210bfc0070f4fa210d6f0818cdd0c3197
magnuax <magaxelsen@gmail.com>
2026-03-10 14:10:19 +0100

    UPDATE: update to flight data test service

--- src/helpers/FlightDataServiceTest.cpp
+++ src/helpers/FlightDataServiceTest.cpp
@@ -1,27 +1,46 @@
 #include "helpers/FlightDataServiceTest.hpp"
 
-FlightDataService::FlightDataService()
+FlightDataServiceTest::FlightDataServiceTest()
 {
     // Initialize with dummy data for testing
     _riskStates = {NORMAL, AT_RISK, CONGESTED, NORMAL};
     _weatherStates = {OK, DEGRADED, SEVERE, EXTREME};
     _trafficStates = {LIGHT, MODERATE, HEAVY, LIGHT};
     _sectorIds = {0, 1, 2, 3};
+    _flightIds = {
+        {101, 102, 103},
+        {201, 202},
+        {301, 302, 303, 304},
+        {401}
+    };
 }
 
+int FlightDataServiceTest::getRowCount() const
+{
+    return 2;
+}
 
-RiskState FlightDataService::getRisk(int sectorId) const
+int FlightDataServiceTest::getColCount() const
+{
+    return 2;
+}
+
+RiskState FlightDataServiceTest::getRisk(int sectorId) const
 {
     return _riskStates[sectorId];
 }
 
-WeatherState FlightDataService::getWeather(int sectorId) const
+WeatherState FlightDataServiceTest::getWeather(int sectorId) const
 {
     return _weatherStates[sectorId];
 }
 
-TrafficState FlightDataService::getTraffic(int sectorId) const
+TrafficState FlightDataServiceTest::getTraffic(int sectorId) const
 {
     return _trafficStates[sectorId];
 }
 
+std::vector<int> FlightDataServiceTest::getFlightIds(int sectorId) const
+{
+    return _flightIds[sectorId];
+}
