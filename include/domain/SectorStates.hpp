ef988e4707283dd2510635a1e29ed80bce00072b
magnuax <magaxelsen@gmail.com>
2026-03-10 14:09:59 +0100

    ADD: converters from sector states to string & icon path

--- include/domain/SectorStates.hpp
+++ include/domain/SectorStates.hpp
@@ -23,6 +23,14 @@ enum TrafficState
     HEAVY,
 };
 
+QString toString(RiskState state);
+QString toString(WeatherState state);
+QString toString(TrafficState state);
+
+QString toIconPath(RiskState state);
+QString toIconPath(WeatherState state);
+QString toIconPath(TrafficState state);
+
 Q_DECLARE_METATYPE(RiskState);
 Q_DECLARE_METATYPE(WeatherState);
 Q_DECLARE_METATYPE(TrafficState);
