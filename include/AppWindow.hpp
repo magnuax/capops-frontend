2d5a88a42e2d99982ff3bc723e7e9c6a0953cf13
magnuax <magaxelsen@gmail.com>
2026-03-11 15:39:29 +0100

    UPDATE: update appwindow to use placeholder websocket client & new MainPage signature

--- include/AppWindow.hpp
+++ include/AppWindow.hpp
@@ -4,24 +4,30 @@
 
 class QWidget;
 class QStackedWidget;
+class WebSocketClient;
+class FlightDataServiceTest;
 class ITileMapService;
-class IFlightDataService;
 
 class AppWindow : public QMainWindow
 {
     Q_OBJECT
 
+public slots:
+    void showMainPage();
+
 public:
     explicit AppWindow(QWidget *parent = nullptr);
 
 private:
+    void initializePages();
+    void initializeServices();
+    void wireServices();
+
     QStackedWidget *_pageStack;
     QWidget *_mainPage;
+
+    FlightDataServiceTest *_dataService;
+    FlightDataServiceTest *_dataEvents;
+    WebSocketClient *_webSocket;
     ITileMapService *_mapFetcher;
-    IFlightDataService *_dataService;
-
-    void initializePages();
-
-public slots:
-    void showMainPage();
 };
--- src/AppWindow.cpp
+++ src/AppWindow.cpp
@@ -5,24 +5,59 @@
 #include "AppWindow.hpp"
 #include "pages/MainPage.hpp"
 
-#include "services/TileMapServiceCARTO.hpp"
+#include "services/WebSocketClient.hpp"
 #include "services/FlightDataServiceTest.hpp"
+#include "services/TileMapServiceCARTO.hpp"
 
 AppWindow::AppWindow(QWidget *parent) : QMainWindow(parent)
 {
-    _dataService = new FlightDataServiceTest();
-
-    _mapFetcher = new TileMapServiceCARTO(this);
-    _mapFetcher->enableDiskCache(QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/maptiles");
+    initializeServices();
+    wireServices();
 
     initializePages();
     showMainPage();
 }
 
+void AppWindow::initializeServices()
+{
+    _webSocket = new WebSocketClient(this);
+    _dataService = new FlightDataServiceTest("./resources/test_data.json");
+    _dataEvents = _dataService;
+    _mapFetcher = new TileMapServiceCARTO(this);
+
+    _mapFetcher->enableDiskCache(QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/maptiles");
+}
+
+void AppWindow::wireServices()
+{
+    // --- Wire WebSocket updates to service ---
+    /*
+
+    connect(_webSocket, &WebSocketClient::sectorRiskReceived,
+            _dataService, &FlightDataServiceWS::updateSectorRisk);
+
+    connect(_webSocket, &WebSocketClient::sectorWeatherReceived,
+            _dataService, &FlightDataServiceWS::updateSectorWeather);
+
+    connect(_webSocket, &WebSocketClient::sectorTrafficReceived,
+            _dataService, &FlightDataServiceWS::updateSectorTraffic);
+
+    connect(_webSocket, &WebSocketClient::sectorFlightsReceived,
+            _dataService, &FlightDataServiceWS::updateSectorFlights);
+
+    // etc...
+
+    */
+}
 
 void AppWindow::initializePages()
 {
-    _mainPage = new MainPage(*_dataService, _mapFetcher, this);
+    _mainPage = new MainPage(
+        *_dataService,
+        _dataEvents,
+        _mapFetcher,
+        this);
+
     _pageStack = new QStackedWidget(this);
     _pageStack->addWidget(_mainPage);
 
