5818c572ef782bd3ae4aa3ef20d17cb4e5a64f84
magnuax <magaxelsen@gmail.com>
2026-03-17 13:56:32 +0100

    UPDATE: updated template for websocket flight data service

--- include/services/FlightDataServiceWS.hpp
+++ include/services/FlightDataServiceWS.hpp
@@ -34,28 +34,21 @@ public:
 
     void bindTo(WebSocketClient *client);
 
-    int getSectorId(int row, int col) const override;
-    std::vector<int> getSectorIds() const override;
-
-    int getRowCount() const override;
-    int getColCount() const override;
-    QSize getGridSize() const override;
-    QPoint getSectorIndices(int sectorId) const override;
-
-    RiskState getRisk(int sectorId) const override;
-    WeatherState getWeather(int sectorId) const override;
-    TrafficState getTraffic(int sectorId) const override;
-
-    std::vector<std::string> getFlightIds(int sectorId) const override;
+    SectorSummaryData getSectorSummaryData() const override;
+    // RiskEventData getRiskEventData() const override;
+    TrackData getTrackData() const override;
 
 private:
     int _rows;
     int _cols;
-    
+
     std::vector<int> _sectorIds;
 
     std::map<sectorId_t, RiskState> _riskStates;
     std::map<sectorId_t, WeatherState> _weatherStates;
     std::map<sectorId_t, TrafficState> _trafficStates;
     std::map<sectorId_t, std::vector<icao24_t>> _flightIds;
+
+    SectorSummaryData *_sectorSummaryData;
+    TrackData *_trackData;
 };
--- src/services/FlightDataServiceWS.cpp
+++ src/services/FlightDataServiceWS.cpp
@@ -22,66 +22,6 @@ void FlightDataServiceWS::bindTo(WebSocketClient *client)
             this, &FlightDataServiceWS::updateSectorFlights);
 }
 
-int FlightDataServiceWS::getSectorId(int row, int col) const
-{
-    // Implementation to retrieve sector ID based on row and column
-    return -1;
-}
-
-std::vector<int> FlightDataServiceWS::getSectorIds() const
-{
-    // Implementation to retrieve all sector IDs
-    return {};
-}
-
-int FlightDataServiceWS::getRowCount() const
-{
-    // Implementation to retrieve the number of rows in the grid
-    return -1;
-}
-
-int FlightDataServiceWS::getColCount() const
-{
-    // Implementation to retrieve the number of columns in the grid
-    return -1;
-}
-
-QSize FlightDataServiceWS::getGridSize() const
-{
-    // Implementation to retrieve the size of the grid
-    return QSize(-1, -1);
-}
-
-QPoint FlightDataServiceWS::getSectorIndices(int sectorId) const
-{
-    // Implementation to retrieve the row and column indices for a given sector ID
-    return QPoint(-1, -1);
-}
-
-RiskState FlightDataServiceWS::getRisk(int sectorId) const
-{
-    // Implementation to retrieve the risk state for a given sector ID
-    return RiskState::NORMAL;
-}
-
-WeatherState FlightDataServiceWS::getWeather(int sectorId) const
-{
-    // Implementation to retrieve the weather state for a given sector ID
-    return WeatherState::OK;
-}
-
-TrafficState FlightDataServiceWS::getTraffic(int sectorId) const
-{
-    // Implementation to retrieve the traffic state for a given sector ID
-    return TrafficState::LIGHT;
-}
-
-std::vector<std::string> FlightDataServiceWS::getFlightIds(int sectorId) const
-{
-    // Implementation to retrieve the list of flight IDs for a given sector ID
-    return {};
-}
-
 void FlightDataServiceWS::updateSectorRisk(int sectorId, RiskState risk)
 {
     if (sectorId >= 0)
@@ -104,4 +44,14 @@ void FlightDataServiceWS::updateSectorFlights(int sectorId, std::vector<std::str
 {
     if (sectorId >= 0)
         _flightIds.at(sectorId) = flightIds;
+}
+
+SectorSummaryData FlightDataServiceWS::getSectorSummaryData() const
+{
+    return *_sectorSummaryData;
+}
+
+TrackData FlightDataServiceWS::getTrackData() const
+{
+    return *_trackData;
 }
