3ad8498e95cac5156ac9bd67c006d6553d5d3bf6
magnuax <magaxelsen@gmail.com>
2026-03-17 09:08:15 +0100

    ADD: risk event entity

---
+++ include/domain/data/RiskEvent.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QDateTime>
+#include <QString>
+
+class RiskEvent
+{
+public:
+    RiskEvent(
+        const int riskEventId,
+        const int sectorId,
+        const bool acknowledged,
+        const QString &riskSeverity,
+        const QDateTime &createdTimestamp,
+        const QDateTime &acknowledgedTimestamp,
+        const QString &message);
+
+    const int getRiskEventId() const;
+    const int getSectorId() const;
+    const bool getAcknowledged() const;
+    const QString &getRiskSeverity() const;
+    const QDateTime &getCreatedTimestamp() const;
+    const QDateTime &getAcknowledgedTimestamp() const;
+    const QString &getMessage() const;
+
+private:
+    int _riskEventId;
+    int _sectorId;
+    bool _acknowledged;
+    QString _riskSeverity;
+    QDateTime _createdTimestamp;
+    QDateTime _acknowledgedTimestamp;
+    QString _message;
+};
